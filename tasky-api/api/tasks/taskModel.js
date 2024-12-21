17504698193994ff1c2e9087f620191653655df6
ShiQingQAQ <20108872@mail.wit.ie>
2024-12-21 08:57:59 +0000

    Mongo lab Users Router

--- tasky-api/api/tasks/taskModel.js
+++ tasky-api/api/tasks/taskModel.js
@@ -6,10 +6,11 @@ const TaskSchema = new Schema({
   title:  { type: String, required: true },
   description:  String ,
   deadline: Date,
-    done: Boolean,
-    priority: { type: String, enum: ["Low", "Medium", "High"], required: true },
-    created_at: Date,
-    updated_at: Date
+  done: Boolean,
+  priority: { type: String, enum: ["Low", "Medium", "High"], required: true },
+  created_at: Date,
+  updated_at: Date,
+  userId: {type: Schema.Types.ObjectId,ref: 'User'}
 });
 
 const dateValidator = (date) => {
--- tasky-api/index.js
+++ tasky-api/index.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import tasksRouter from './api/tasks';
 import './db';
-
+import usersRouter from './api/users';
 
 dotenv.config();
 
@@ -22,6 +22,9 @@ const port = process.env.PORT;
 app.use(express.json());
 
 app.use('/api/tasks', tasksRouter);
+
+app.use('/api/users', usersRouter);
+
 
 app.use(errHandler);
 
